30b11e6b884fe9f099d4c51755583890704b38f2
Viktoriya08 <victoria.gnezdilova@gmail.com>
2021-01-15 23:22:44 +0300

    Добавлена логика раскрытия основного меню и Picture для разных разрешений экрана

---
+++ source/js/menuHandler.js
@@ -0,0 +1,13 @@
+const mainNavButton = document.querySelector(".main-nav__toggle");
+const header = document.querySelector(".page-header");
+const mainNav = document.querySelector(".main-nav");
+
+function menuToggle() {
+  mainNavButton.addEventListener("click", function () {
+    header.classList.toggle("page-header--opened");
+    mainNav.classList.toggle("main-nav--opened");
+    mainNav.classList.toggle("main-nav--closed");
+  });
+}
+
+menuToggle();
